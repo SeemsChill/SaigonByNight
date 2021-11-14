2c6dc7062a281658a6e16bb5cbc063876fdb6f21
SeemsChill <ilovesaigonbefore1975@gmail.com>
2021-11-14 23:14:11 +0700

    Ready to deploy to Vercel.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'https://saigon-by-night-server.herokuapp.com/:path*',
-          },
-        ]
-      }
+  
 };
